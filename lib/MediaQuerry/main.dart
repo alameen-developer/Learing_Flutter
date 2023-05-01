8dad22bee5e11d437db667452ac216246fbda974
alameenalim <alameenalim007@gmail.com>
2023-05-01 17:45:16 +0530

    Initial commit

---
+++ lib/MediaQuerry/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tryflutter/Homepage.dart';
+import 'package:tryflutter/MediaQuerry/homepage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
+}
